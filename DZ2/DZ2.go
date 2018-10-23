8ff2640a355a95e001019c70658d435c17d07530
Nikita Homenko <44022430+nik27090@users.noreply.github.com>
2018-10-23 03:19:22 +0300

    Update DZ2.go

--- DZ2/DZ2.go
+++ DZ2/DZ2.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"github.com/polis-mail-ru-golang-1/t2-invert-index-search-nik27090/tree/master/DZ2/funcs"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/polis-mail-ru-golang-1/t2-invert-index-search-nik27090/DZ2/funcs"
 )
 
 func main() {
 	//срез файлов(название и содержание)
-	files := funcs.OpenFiles()
+	files := openFiles()
 
 	//ввод фразы с консоли, создание среза
-	phrases := funcs.СreatePhrase()
+	phrases := createPhrase()
 
 	//инвертированный индекс
 	inIn := funcs.InvertIndex(files)
@@ -19,4 +25,38 @@ func main() {
 
 	//сортировка файлов по большему кол-ву сопадений
 	funcs.SortSearch(end)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func scanStr() string {
+	in := bufio.NewScanner(os.Stdin)
+	in.Scan()
+	if err := in.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
+	}
+	return in.Text()
+}
+
+func openFiles() []funcs.File {
+	fileNames := os.Args[1:]
+	sliceFiles := make([]funcs.File, 0)
+	for _, names := range fileNames {
+		file, err := ioutil.ReadFile(names)
+		check(err)
+		f := funcs.File{Name: names, Content: string(file)}
+		sliceFiles = append(sliceFiles, f)
+	}
+	return sliceFiles
+}
+
+func createPhrase() []string {
+	fmt.Println("Введите фразу:")
+	phrase := scanStr()
+	slicePhrase := strings.Split(phrase, " ")
+	return slicePhrase
 }
