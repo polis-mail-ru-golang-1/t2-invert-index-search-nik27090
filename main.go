44c87709ba88704b727dfc591492370cf9385087
Nikita Homenko <44022430+nik27090@users.noreply.github.com>
2018-11-03 00:17:03 +0300

    Добавил интерфейс HTTP

--- main.go
+++ main.go
@@ -1,43 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
-	"sync"
 
-	"github.com/polis-mail-ru-golang-1/t2-invert-index-search-nik27090/DZ2/funcs"
+	"./invertIndex"
 )
 
-func main() {
-	inIn := make(map[string]map[string]int)
+func handler(w http.ResponseWriter, r *http.Request) {
+	inIn, sliceFiles := openFiles(w)
 
-	//срез файлов(название и содержание)
-	files := openFiles()
-
-	//ввод фразы с консоли, создание среза
-	phrases := createPhrase()
-
-	//инвертированный индекс
-	var wg sync.WaitGroup
-	wg.Add(len(files))
-
-	var mutex sync.Mutex
-
-	for i := 0; i < len(files); i++ {
-		go funcs.InvertIndexGo(inIn, files[i].Name, files[i].Content, &wg, &mutex)
+	q := r.FormValue("q")
+	if q != "" {
+		endMap := find(inIn, q, sliceFiles)
+		sortSearch(endMap, w)
 	}
+}
 
-	wg.Wait()
-
-	//срез с файлами в которых поисковая фраза содержиться полностью
-	end := funcs.Find(inIn, phrases, files)
-
-	//сортировка файлов по большему кол-ву сопадений
-	//и вывод результата в консоль
-	funcs.SortSearch(end)
+func main() {
+	http.HandleFunc("/search", handler)
+	fmt.Println("Server is listening...")
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
 
 func check(e error) {
@@ -46,31 +32,109 @@ func check(e error) {
 	}
 }
 
-func scanStr() string {
-	in := bufio.NewScanner(os.Stdin)
-	in.Scan()
-	if err := in.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
-	}
-	return in.Text()
-}
+func openFiles(w http.ResponseWriter) (map[string]map[string]int, []invertIndex.File) {
+	sliceFiles := make([]invertIndex.File, 0)
+	directoryFiles := os.Args[1]
 
-func openFiles() []funcs.File {
-	fileNames := os.Args[1:]
-	sliceFiles := make([]funcs.File, 0)
-	for _, names := range fileNames {
-		file, err := ioutil.ReadFile(names)
+	sliceFileInfo, err := ioutil.ReadDir(directoryFiles)
+	check(err)
+
+	if len(sliceFileInfo) == 0 {
+		fmt.Fprintf(w, "Files not found\n")
+	}
+
+	for i := 0; i < len(sliceFileInfo); i++ {
+		dirFile := directoryFiles + "/" + sliceFileInfo[i].Name()
+		textFile, err := ioutil.ReadFile(dirFile)
 		check(err)
-		f := funcs.File{Name: names, Content: string(file)}
+		f := invertIndex.File{Name: sliceFileInfo[i].Name(), Content: string(textFile)}
 		sliceFiles = append(sliceFiles, f)
 	}
-	return sliceFiles
+	return invertIndex.PreInvertIndex(sliceFiles), sliceFiles
 }
 
-func createPhrase() []string {
-	fmt.Println("Введите фразу:")
-	phrase := scanStr()
-	slicePhrase := strings.Split(phrase, " ")
-	return slicePhrase
+func find(inIn map[string]map[string]int, q string, sliceFiles []invertIndex.File) map[string]int {
+	q = strings.ToLower(q)
+	phrase := strings.Split(q, " ")
+	phWords := mapWithQWord(inIn, phrase)
+	goodFile := takeGoodFile(inIn, sliceFiles, phrase)
+	endMap := make(map[string]int)
+	//добавляет число совпадений слов поисковой фразы с текстом файла
+	for _, gFile := range goodFile {
+		for _, item := range phWords {
+			for name, i := range item {
+				if gFile == name {
+					endMap[name] = endMap[name] + i
+				}
+			}
+		}
+	}
+	return endMap
 }
+
+//уменьшает ИнвИнд до имеющихся слов в поисковой фразе
+func mapWithQWord(inIn map[string]map[string]int, phrase []string) map[string]map[string]int {
+	phWords := make(map[string]map[string]int)
+	for fileWord, _ := range inIn {
+		for _, findWord := range phrase {
+			if fileWord == findWord {
+				phWords[findWord] = inIn[findWord]
+			}
+		}
+	}
+	return phWords
+}
+
+//создает срез файлов имеющих поисковую фразу полностью
+func takeGoodFile(inIn map[string]map[string]int, sliceFiles []invertIndex.File, phrase []string) []string {
+	s := 0
+	goodFile := make([]string, 0)
+	for _, file := range sliceFiles {
+		for _, ph := range phrase {
+			if _, ok := inIn[ph][file.Name]; ok {
+				if inIn[ph][file.Name] != 0 {
+					s++
+				}
+			} else {
+				continue
+			}
+		}
+		if s == len(phrase) {
+			goodFile = append(goodFile, file.Name)
+		}
+		s = 0
+	}
+	return goodFile
+}
+
+func sortSearch(endMap map[string]int, w http.ResponseWriter) {
+	if len(endMap) == 0 {
+		fmt.Fprintf(w, "По вашему запросу ничего не найдено")
+		return
+	}
+	bufName := ""
+	bufCount := 0
+	nameFile := make([]string, 0)
+	count := make([]int, 0)
+	for name, c := range endMap {
+		nameFile = append(nameFile, name)
+		count = append(count, c)
+	}
+	for i := 0; i < len(nameFile); i++ {
+		for j := i; j < len(nameFile); j++ {
+			if count[i] < count[j] {
+				bufName = nameFile[i]
+				nameFile[i] = nameFile[j]
+				nameFile[j] = bufName
+				bufCount = count[i]
+				count[i] = count[j]
+				count[j] = bufCount
+			}
+		}
+	}
+	for i := 0; i < len(nameFile); i++ {
+		fmt.Fprintf(w, "- %s; совпадений - %d\n", nameFile[i], count[i])
+	}
+}
+
 
