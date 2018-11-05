ad47788b8ed7de90b09c7f0ca712a574d2414718
Nikita Homenko <44022430+nik27090@users.noreply.github.com>
2018-11-05 21:34:18 +0300

    main() -> init()

--- invertIndex/invertIndex(B)_test.go
+++ invertIndex/invertIndex(B)_test.go
@@ -8,12 +8,11 @@ import (
 
 var sliceFiles2 []File
 
-func main() {
+func init() {
 	f := File{Name: "time.txt", Content: ""}
 	file, _ := ioutil.ReadFile(f.Name)
 	f.Content = string(file)
 	sliceFiles2 = append(sliceFiles2, f)
-	fmt.Println(sliceFiles2)
 }
 
 func BenchmarkInvertInd(b *testing.B) {
